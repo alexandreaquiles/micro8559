bc46bf47d096b037ece3613856d3fe0d792cd095
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2019-08-02 16:09:21 -0300

    Exercícios Cap 14 (Monitoramento e Observabilidade) 14.1, 14.2, 14.3, 14.4, 14.5, 14.6, 14.7 Health Check API com Spring Boot Actuator, Visualização de Circuit Breakers com Hystrix Dashboard, Agregando dados de Circuit Breakers com Turbine, Agregação baseada em mensagens com Turbine Stream, Zipkin e Spring Cloud Sleuth

---
+++ turbine/src/main/java/br/com/caelum/turbine/TurbineApplication.java
@@ -0,0 +1,15 @@
+package br.com.caelum.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+
+@EnableTurbineStream
+@SpringBootApplication
+public class TurbineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurbineApplication.class, args);
+	}
+
+}
